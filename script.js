5ba4566d56c5b19596e165f4dea4848dc9a4b9d6
Samir Bilal Memon <samirbmemon@gmail.com>
2021-05-06 09:13:00 +0000

    done with project

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    // Used a less then or equal to 0 rule and returned false to prevent computation without principal
+    if (principal <= 0) { alert("Enter a positive number"); document.getElementById("principal").focus(); return false; }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+    // Created amount variable for result
+    var amount = interest * principal;
+    // Used span elements and created new css rule to highlight numbers in results
+    document.getElementById("result").innerHTML="If you deposit \<span class=highlight>"+principal+"\</span>,\<br\>at an interest rate of \<span class=highlight>"+rate+"%\</span>\<br\>You will receive an amount of \<span class=highlight>"+amount+"\</span>,\<br\>in the year \<span class=highlight>"+year+"\</span>\<br\>"
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
